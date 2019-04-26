18e0c51b0f6e7f782aa8b4d0d30dae1b1292bef1
einstein1967 <bootcamp.ejb@gmail.com>
2019-04-26 12:42:07 -0500

    final commit?

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,79 +8,80 @@ var win = 0;
 var lose = 0;
 var nogo = 0;
 var result = "";
+$(document).ready(function () {
 
-// Choose random word from array.
-var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    // Choose random word from array.
+    var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
 
-// Create array from random word.
-var lettersArray = randomWord.split("");
-console.log("random word is " + lettersArray);
+    // Create array from random word.
+    var lettersArray = randomWord.split("");
+    console.log("random word is " + lettersArray);
+
+    $("#playAgain").hide();
 
 
-
-
-// Create the word grid the user sees.
-for (var i = 0; i < lettersArray.length; i++) {
-    outputArray[i] = "_";
-}
-
-console.log(outputArray);
-$("#current").text(outputArray).addClass("box-text");
-$("#used").text(alphaArray).addClass("box-text");
-
-// // Main game event.
-document.onkeyup = function (event) {
-    var userGuess = event.key.toUpperCase();
-
-    nogo = 0;
-
-    if (result === "") {
-        $('h3').hide();
-    }else{
-        $('h3').show();
-    }
-
-    // Makes sure letter hasn't already been used.
-    for (var i = 0; i < alphaArray.length; i++) {
-        if (userGuess === alphaArray[i]) {
-            return;
-        }
-    }
-
-    alphaArray.push(userGuess);
-
-    var output = $('output');
-
-    // Sees if guessed letter is in word and determines if word is complete.
+    // Create the word grid the user sees.
     for (var i = 0; i < lettersArray.length; i++) {
-        if (userGuess === lettersArray[i]) {
-            outputArray[i] = userGuess;
-            win++;
-        } else {
-            nogo++;
-        }
+        outputArray[i] = "_";
     }
 
-    if (nogo === lettersArray.length) {
-        lose++;
-    }
-
-    // console.log("wrong guess total = " + lose);
-    $("#graphic").attr("src", imageArray[lose]);
-
+    console.log(outputArray);
     $("#current").text(outputArray).addClass("box-text");
     $("#used").text(alphaArray).addClass("box-text");
 
+    // // Main game event.
 
-    if (lose === 10) {
-        var result = "Sorry, you lose.";
-        // How do I stop the event?
+    document.onkeyup = function (event) {
+        var userGuess = event.key.toUpperCase();
+
+        nogo = 0;
+
+        // Makes sure letter hasn't already been used.
+        for (var i = 0; i < alphaArray.length; i++) {
+            if (userGuess === alphaArray[i]) {
+                return;
+            }
+        }
+
+        alphaArray.push(" " + userGuess);
+        console.log(alphaArray);
+
+        var output = $('output');
+
+        // Sees if guessed letter is in word and determines if word is complete.
+        for (var i = 0; i < lettersArray.length; i++) {
+            if (userGuess === lettersArray[i]) {
+                outputArray[i] = userGuess;
+                win++;
+            } else {
+                nogo++;
+            }
+        }
+
+        if (nogo === lettersArray.length) {
+            lose++;
+        }
+
+        // console.log("wrong guess total = " + lose);
+        $("#graphic").attr("src", imageArray[lose]);
+
+        $("#current").text(outputArray).addClass("box-text");
+        $("#used").text(alphaArray).addClass("box-text");
+
+
+        if (lose === 10) {
+            $("#result").text("And the crowd was right.");
+            $("#playAgain").show();
+
+            // How do I stop the event?
+        }
+
+        if (win === lettersArray.length) {
+            $("#result").text("You won!");
+            $("#playAgain").show();
+
+            // How do I stop the event?
+        }
     }
 
-    if (win === lettersArray.length) {
-        var result = "You win!";
-        // How do I stop the event?
-    }
-
-
-}
+});
